d109dac32888e76ae09d10792c100d5f64130ef9
Lostark13 <tushar9915@gmail.com>
2026-07-25 11:42:15 +0000

    sky: Update Product shipping api level
    as per stock

--- device.mk
+++ device.mk
@@ -387,7 +387,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_ravelin/android.hardware.sensor.stepdetector.xml
 
 # SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
